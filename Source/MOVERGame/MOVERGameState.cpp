311dc70f7298cc854b7bf9219bed09dd99c25a3c
Kyle Bauer <kbau121@gmail.com>
2024-10-27 00:06:53 -0400

    Fixed build errors

--- Source/MOVERGame/MOVERGameState.cpp
+++ Source/MOVERGame/MOVERGameState.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include <Net/UnrealNetwork.h>
-
 #include "MOVERGameState.h"
+
+#include <Net/UnrealNetwork.h>
 
 AMOVERGameState::AMOVERGameState(const FObjectInitializer& ObjectInitializer)
 	: UIStateUpdated(),
